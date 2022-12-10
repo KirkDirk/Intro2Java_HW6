99f6b7ce08bb1160e268e1564c49ea028e014c0a
Kirk Dirk <jackalex@ya.ru>
2022-12-10 22:19:14 +0500

    add 1, 2

--- hw6_main.java
+++ hw6_main.java
@@ -1,14 +1,35 @@
 // Реализовать множество (set) с помощью TreeMap. 
 // Создать метод add, добавляющий элемент в ваше множество. 
-// Объявить и инициализировать компаратор с обратной (от большего к меньшеиу) логикой сортировки чисел. 
+// Объявить и инициализировать компаратор с обратной (от большего к меньшему) логикой сортировки чисел. 
 // Объявить и инициализировать TreeSet использующий ваш компаратор. 
 // Добавить несколько элементов в сет и вывести в консоль.
 
-
 package date221121.HW6;
 
-public class hw6_main {
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Hw6_main {
     public static void main(String[] args) {
+        Hw6_someset someSet = new Hw6_someset();
+        boolean moreItems = false;
         
+        Scanner scanMore = new Scanner(System.in);
+        System.out.println("Добавить элемент в someSet? Y/N:");
+        if (scanMore.nextLine().toLowerCase().equals("y")) moreItems = true;
+        System.out.println(moreItems);
+        while (moreItems) {
+            Scanner scanItem = new Scanner(System.in);
+            System.out.print("Введите Id: ");
+            Integer itemId = Integer.parseInt(scanItem.nextLine());
+            System.out.print("Введите Str: ");
+            String itemStr = scanItem.nextLine();
+            someSet.addItem(itemId, itemStr); 
+            System.out.println("Добавить элемент в someSet? Y/N:");
+            if (scanMore.nextLine().toLowerCase().equals("y")) moreItems = true;  
+            else moreItems = false;         
+        }
+        someSet.printSet();
     }
 }
--- hw6_someset.java
+++ hw6_someset.java
@@ -1,9 +1,31 @@
 package date221121.HW6;
 
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.TreeMap;
 
-public class hw6_someset {
-    Map<Integer, String> SomeTreeMap = new TreeMap<>();
+public class Hw6_someset {
+    Map<Integer, String> someTreeMap = new TreeMap<>();
+
+    public void addItem(Integer addId, String addStr) {
+        if (someTreeMap.containsKey(addId)) { //проверяем совпадение ID
+            System.out.println("Такой id уже существует. Заменить значение? Y/N:");
+            Scanner scanYN = new Scanner(System.in);
+            String rewriteStr = scanYN.nextLine().toLowerCase();
+            if (rewriteStr == "y") someTreeMap.replace(addId, addStr);
+            else System.out.println("Значение не было добалено");
+            scanYN.close();
+        }
+        else someTreeMap.put(addId, addStr);
+    }
+
+    public void printSet() {
+        Iterator<Map.Entry<Integer, String>> setIt = someTreeMap.entrySet().iterator();
+        while (setIt.hasNext()) {
+            Map.Entry<Integer, String> item = setIt.next();
+            System.out.println(item.getKey() + ": " + item.getValue());
+        }
+    }
 }
 
