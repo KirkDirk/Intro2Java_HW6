ade28a48c7da446dce48c994a59ad29f8a4d818b
Kirk Dirk <jackalex@ya.ru>
2022-12-11 19:19:32 +0500

    add 3-5

--- hw6_main.java
+++ hw6_main.java
@@ -1,16 +1,18 @@
-// Реализовать множество (set) с помощью TreeMap. 
-// Создать метод add, добавляющий элемент в ваше множество. 
-// Объявить и инициализировать компаратор с обратной (от большего к меньшему) логикой сортировки чисел. 
-// Объявить и инициализировать TreeSet использующий ваш компаратор. 
-// Добавить несколько элементов в сет и вывести в консоль.
+// 1) Реализовать множество (set) с помощью TreeMap. 
+// 2) Создать метод add, добавляющий элемент в ваше множество. 
+// 3) Объявить и инициализировать компаратор с обратной (от большего к меньшему) логикой сортировки чисел. 
+// 4) Объявить и инициализировать TreeSet использующий ваш компаратор. 
+// 5) Добавить несколько элементов в сет и вывести в консоль.
 
 package date221121.HW6;
 
-import java.util.Iterator;
-import java.util.Map;
+import java.util.Comparator;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Hw6_main {
+    //1) и 2)
     public static void main(String[] args) {
         Hw6_someset someSet = new Hw6_someset();
         boolean moreItems = false;
@@ -18,7 +20,7 @@ public class Hw6_main {
         Scanner scanMore = new Scanner(System.in);
         System.out.println("Добавить элемент в someSet? Y/N:");
         if (scanMore.nextLine().toLowerCase().equals("y")) moreItems = true;
-        System.out.println(moreItems);
+        //nSystem.out.println(moreItems);
         while (moreItems) {
             Scanner scanItem = new Scanner(System.in);
             System.out.print("Введите Id: ");
@@ -31,5 +33,22 @@ public class Hw6_main {
             else moreItems = false;         
         }
         someSet.printSet();
+        scanMore.close();
+
+        //3), 4) и
+        Comparator<Integer> compaInt = new Comparator<>() {
+            @Override
+            public int compare(Integer a, Integer b) {
+                return b.compareTo(a);
+            }
+        };
+
+        Set<Integer> trSet = new TreeSet<>(compaInt);
+        trSet.add(5);
+        trSet.add(8);
+        trSet.add(15);
+        trSet.add(3);
+        trSet.add(7);
+        System.out.println(trSet);
     }
 }
--- hw6_someset.java
+++ hw6_someset.java
@@ -1,13 +1,14 @@
 package date221121.HW6;
 
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.TreeMap;
 
 public class Hw6_someset {
-    Map<Integer, String> someTreeMap = new TreeMap<>();
-
+    TreeMap<Integer, String> someTreeMap = new TreeMap<>();
+    
     public void addItem(Integer addId, String addStr) {
         if (someTreeMap.containsKey(addId)) { //проверяем совпадение ID
             System.out.println("Такой id уже существует. Заменить значение? Y/N:");
